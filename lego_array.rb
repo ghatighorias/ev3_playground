d85631da45546a7effcbd7754daa4fd4e4b4be3c
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2016-11-11 10:40:37 +0100

    adding next functionality

--- lego_array.rb
+++ lego_array.rb
@@ -4,6 +4,20 @@ require './color_sensor'
 class LegoArray
   attr_reader :motor, :color_sensor, :array
 
+  def moveFromBlackToWhite
+    while current_color == 'Black'
+      motor.move_forward
+      current_color = color_sensor.current_color
+    end
+  end
+
+  def moveFromWhiteToBlack
+    while current_color == 'White'
+      motor.move_forward
+      current_color = color_sensor.current_color
+    end
+  end
+
   def initialize(array)
     @motor = Motor.new
     @color_sensor = ColorSensor.new
@@ -11,8 +25,12 @@ class LegoArray
   end
 
   def next
+    moveFromBlackToWhite
+
+    moveFromWhiteToBlack
   end
 
   def previous
   end
+
 end
