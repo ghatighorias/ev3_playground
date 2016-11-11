0beb129230ac71bccde12ce3c44cc3397f9fec97
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2016-11-11 11:13:17 +0100

    introduces go_to functionality

--- lego_array.rb
+++ lego_array.rb
@@ -28,24 +28,37 @@ class LegoArray
     end
   end
 
-  def initialize(array)
+  def initialize(array, starting_position) #{-1 .. 4}
     @motor = Motor.new
     @color_sensor = ColorSensor.new
     @array = array
-
+    @current_array_index = starting_position
   end
 
-  def next
+  def next_position
     move_from_black_to_white(true)
-
     move_from_white_to_black(true)
   end
 
-  def previous
+  def previous_position
     move_from_black_to_white(false)
-
     move_from_white_to_black(false)
   end
 
+ def go_to(index)
+   steps = index - @current_array_index
+
+   if (steps < 0)
+     steps.abs.times do
+       previous_position
+     end
+   else
+     steps.times do
+       next_position
+     end
+   end
+
+  @current_array_index = index
+ end
 
 end
