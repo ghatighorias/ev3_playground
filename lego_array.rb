3e2cb7c6069610bc47be8468c65e01e5d87a6a53
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2016-11-11 12:53:25 +0100

    refactoring

--- lego_array.rb
+++ lego_array.rb
@@ -1,8 +1,9 @@
 require './motor'
 require './color_sensor'
 require './movement_helper'
+
 class LegoArray
-  attr_reader :motor, :color_sensor, :array
+  attr_reader :array
 
   def initialize(array, starting_position)
     @array = array
@@ -10,18 +11,19 @@ class LegoArray
     @current_array_index = starting_position
   end
 
-  def go_to(index)
+  def go_to(requested_index)
     return unless is_index_in_range(index)
 
-    steps = abs(index - @current_array_index)
-    direction_is_forward = is_direction_forward(index, @current_array_index)
-    @movement_helper.change_position_in_direction(direction_is_forward, steps)
+    move_safely = true
+    steps = abs(requested_index - @current_array_index)
+    is_forward = is_direction_forward(requested_index, @current_array_index)
+    @movement_helper.change_position_in_direction(is_forward, steps, move_safely)
 
-    @current_array_index = index
+    @current_array_index = requested_index
   end
 
   def is_index_in_range(index)
-    return if index >= -1 and index < @array.length #@array.length - 1
+    return if index >= -1 and index < @array.length
   end
 
   def is_direction_forward(requested_index, current_index)
--- movement_helper.rb
+++ movement_helper.rb
@@ -23,11 +23,11 @@ class MovementHelper
     end
   end
 
-  def move_from_black_to_white(forward)
+  def move_unless_see_color(forward, color)
     steps = 0;
     current_color = color_sensor.current_color
 
-    while current_color != 'White'
+    while current_color != color
       move_in_direction(forward)
       steps++
       current_color = color_sensor.current_color
@@ -36,17 +36,12 @@ class MovementHelper
     return steps;
   end
 
+  def move_from_black_to_white(forward)
+    return move_till_color(forward, 'White')
+  end
+
   def move_from_white_to_black(forward)
-    steps = 0;
-    current_color = color_sensor.current_color
-
-    while current_color != 'Black'
-      move_in_direction(forward)
-      steps++
-      current_color = color_sensor.current_color
-    end
-
-    return steps
+    return move_till_color(forward, 'Black')
   end
 
   def safe_move_to_black(forward)
@@ -55,22 +50,30 @@ class MovementHelper
     move_in_direction_times(!forward, (steps / 2))
   end
 
-  def next_position
+  def next_position(move_safely)
     move_from_black_to_white(true)
-    safe_move_to_black(true)
+
+    if(move_safely)
+      safe_move_to_black(true)
+    else
+      move_from_white_to_black(true)
   end
 
-  def previous_position
+  def previous_position(move_safely)
     move_from_black_to_white(false)
-    safe_move_to_black(false)
+
+    if(move_safely)
+      safe_move_to_black(false)
+    else
+      move_from_white_to_black(false)
   end
 
-  def change_position_in_direction(direction_is_forward, change_time)
+  def change_position_in_direction(direction_is_forward, change_time, move_safely)
     change_time.times do
       if direction_is_forward
-        next_position
+        next_position(move_safely)
       else
-        previous_position
+        previous_position(move_safely)
       end
     end
   end
