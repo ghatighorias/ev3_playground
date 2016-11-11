fa44d6a775c1ee4a5505b4b225739e846adb284a
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2016-11-11 12:27:20 +0100

    refactoring the code

--- lego_array.rb
+++ lego_array.rb
@@ -1,76 +1,24 @@
 require './motor'
 require './color_sensor'
-
+require './movement_helper'
 class LegoArray
   attr_reader :motor, :color_sensor, :array
 
-  def move_from_black_to_white(toForward = true)
-    current_color = color_sensor.current_color
-    while current_color != 'White'
-      if toForward
-        motor.move_forward
-      else
-        motor.move_backward
-      end
-      current_color = color_sensor.current_color
-    end
-  end
-
-  def move_from_white_to_black(toForward = true)
-    current_color = color_sensor.current_color
-    while current_color != 'Black'
-      if toForward
-        motor.move_forward
-      else
-        motor.move_backward
-      end
-      current_color = color_sensor.current_color
-    end
-
-    if toForward
-      motor.move_forward
-      motor.move_forward
-      motor.move_forward
-    else
-      motor.move_backward
-      motor.move_backward
-      motor.move_backward
-    end
-  end
-
-  def initialize(array, starting_position) #{-1 .. 4}
-    @motor = Motor.new
-    @color_sensor = ColorSensor.new
+  def initialize(array, starting_position)
+    @movement_helper = MovementHelper.new
     @array = array
     @current_array_index = starting_position
   end
 
-  def next_position
-    move_from_black_to_white(true)
-    move_from_white_to_black(true)
+  def go_to(index)
+    return if index < -1 and index >= @array.length
+
+    steps = abs(index - @current_array_index)
+    direction_is_forward = index > @current_array_index
+
+    @movement_helper.change_position_in_direction(direction_is_forward, steps)
+
+    @current_array_index = index
   end
-
-  def previous_position
-    move_from_black_to_white(false)
-    move_from_white_to_black(false)
-  end
-
- def go_to(index) #3 is okay, 1 is not
-   return if index < -1 and index >= @array.length
-
-   steps = index - @current_array_index
-
-   if (steps < 0)
-     steps.abs.times do
-       previous_position
-     end
-   else
-     steps.times do
-       next_position
-     end
-   end
-
-  @current_array_index = index
- end
 
 end
---
+++ movement_helper.rb
@@ -0,0 +1,73 @@
+require './motor'
+require './color_sensor'
+
+class MovementHelper
+  attr_reader :motor, :color_sensor, :array
+
+  def initialize()
+    @motor = Motor.new
+    @color_sensor = ColorSensor.new
+  end
+
+  def move_in_direction(forward)
+    if forward
+      motor.move_forward
+    else
+      motor.move_backward
+    end
+  end
+
+  def move_in_direction_times(forward, cycle)
+    cycle.times do
+      move_in_direction(forward)
+    end
+  end
+
+  def move_from_black_to_white(forward)
+    current_color = color_sensor.current_color
+    steps = 0;
+    while current_color != 'White'
+      move_in_direction(forward)
+      steps++
+      current_color = color_sensor.current_color
+    end
+    return steps;
+  end
+
+  def move_from_white_to_black(forward)
+    current_color = color_sensor.current_color
+    steps = 0;
+    while current_color != 'Black'
+      move_in_direction(forward)
+      steps++
+      current_color = color_sensor.current_color
+    end
+    return steps
+  end
+
+  def safe_move_to_black(forward)
+    move_from_white_to_black(forward)
+    steps = move_from_black_to_white(forward)
+    move_in_direction_times(!forward, (steps / 2))
+  end
+
+  def next_position
+    move_from_black_to_white(true)
+    safe_move_to_black(true)
+  end
+
+  def previous_position
+    move_from_black_to_white(false)
+    safe_move_to_black(false)
+  end
+
+  def change_position_in_direction(direction_is_forward, change_time)
+    change_time.times do
+      if direction_is_forward
+        next_position
+      else
+        previous_position
+      end
+    end
+  end
+end
